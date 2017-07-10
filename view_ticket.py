79944f9166ebc723f02e102ae15431bd67d6c553
David Zisky <dawid.ziolkowski@klarrio.com>
2017-07-10 15:41:04 +0200

    Update view_ticket.py
    Most important informations printed

--- view_ticket.py
+++ view_ticket.py
@@ -3,18 +3,27 @@
 
 import requests
 import json
+import sys
 
 api_key = "YOUR_API_KEY"
 domain = "YOUR_DOMAIN"
 password = "x"
 
 # Id of the ticket to be updated
-ticket_id = 'TICKET_ID'
+ticket_id = str(sys.argv[1])
 
 r = requests.get("https://"+ domain +".freshdesk.com/api/v2/tickets/"+ticket_id, auth = (api_key, password))
 
 if r.status_code == 200:
-  print "Request processed successfully, the response is given below" + r.content
+  #print "Request processed successfully, the response is given below" + r.content
+  
+  print("---------")
+  print "ID: " + str(parsed["id"])
+  print "Subject: " + parsed["subject"]
+  print "Description: " + parsed["description_text"]
+  print "Emails: " + str(parsed["cc_emails"])
+  print("---------")
+  
 else:
   print "Failed to read ticket, errors are displayed below,"
   response = json.loads(r.content)
