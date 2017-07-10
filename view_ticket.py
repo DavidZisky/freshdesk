618c11f881911d3d6eab35aa2e3b0b0601e9de3b
David Zisky <dawid.ziolkowski@klarrio.com>
2017-07-10 15:45:02 +0200

    Update view_ticket.py

--- view_ticket.py
+++ view_ticket.py
@@ -16,7 +16,7 @@ r = requests.get("https://"+ domain +".freshdesk.com/api/v2/tickets/"+ticket_id,
 
 if r.status_code == 200:
   #print "Request processed successfully, the response is given below" + r.content
-  
+  parsed = json.loads(r.content)
   print("---------")
   print "ID: " + str(parsed["id"])
   print "Subject: " + parsed["subject"]
